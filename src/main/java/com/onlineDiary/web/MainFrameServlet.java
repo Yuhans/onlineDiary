7c433093954b630158ada5228ddbfe8d658e226b
Yuhans <yushin.tema@gmail.com>
2016-11-27 20:28:51 +0300

    trying cookies

--- src/main/java/com/onlineDiary/web/MainFrameServlet.java
+++ src/main/java/com/onlineDiary/web/MainFrameServlet.java
@@ -6,6 +6,7 @@ import com.onlineDiary.logic.Student;
 import com.onlineDiary.web.forms.MainFrameForm;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,36 +19,42 @@ public class MainFrameServlet extends HttpServlet {
     private final ManagementSystem dao = ManagementSystem.getInstance();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("classes", dao.getClasses());
-        MainFrameForm form = new MainFrameForm();
-        int classId;
-        classId = Integer.parseInt(request.getParameter("stClass"));
-        List<SClass> classes = dao.getClasses();
-        SClass schoolClass = new SClass();
-        schoolClass.setClassId(classId);
-        if (classId == -1) {
-            Iterator i = classes.iterator();
-            schoolClass = (SClass) i.next();
-        }
-        List<Student> students = dao.getStudentsByClass(classId);
-        form.setClassId(schoolClass.getClassId());
-        form.setsClasses(classes);
-        form.setStudents(students);
-        request.setAttribute("form", form);
-        if (request.getParameter("stClass") != null) {
-            request.setAttribute("students", dao.getStudentsByClass(classId));
-            schoolClass = dao.getClasses().get(classId - 1);
-            request.setAttribute("subjects", dao.getSubjects(schoolClass.getStudyYear()));
-        }
-        if (request.getParameter("Log out") != null) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            request.setAttribute("classes", dao.getClasses());
+            MainFrameForm form = new MainFrameForm();
+            int classId;
+            classId = Integer.parseInt(request.getParameter("stClass"));
+            List<SClass> classes = dao.getClasses();
+            SClass schoolClass = new SClass();
+            schoolClass.setClassId(classId);
+            if (classId == -1) {
+                Iterator i = classes.iterator();
+                schoolClass = (SClass) i.next();
+            }
+            List<Student> students = dao.getStudentsByClass(classId);
+            form.setClassId(schoolClass.getClassId());
+            form.setsClasses(classes);
+            form.setStudents(students);
+            request.setAttribute("form", form);
+            if (request.getParameter("stClass") != null) {
+                request.setAttribute("students", dao.getStudentsByClass(classId));
+                schoolClass = dao.getClasses().get(classId - 1);
+                request.setAttribute("subjects", dao.getSubjects(schoolClass.getStudyYear()));
+            }
+            request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
+        } else {
             response.sendRedirect("/auth");
-            return;
         }
-        request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("classes", dao.getClasses());
-        request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            request.setAttribute("classes", dao.getClasses());
+            request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/auth");
+        }
     }
 }
