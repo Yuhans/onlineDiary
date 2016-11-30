3e11d40a6faf55cede19e8af7c63c5dc6dec9470
Yuhans <yushin.tema@gmail.com>
2016-11-30 16:42:01 +0300

    add switch in AuthServlet

--- src/main/java/com/onlineDiary/web/AuthPageServlet.java
+++ src/main/java/com/onlineDiary/web/AuthPageServlet.java
@@ -4,52 +4,50 @@ import com.onlineDiary.logic.ManagementSystem;
 import com.onlineDiary.logic.User;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.SQLException;
 
 
 public class AuthPageServlet extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest req, HttpServletResponse resp)
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        int answer;
-        try {
-            answer = checkAction(req);
-        } catch (SQLException sql_e) {
-            throw new IOException(sql_e.getMessage());
+        int answer = checkAction(req);
+        switch (answer) {
+            case 1:
+                if (req.getParameter("login") != null) {
+                    String login = req.getParameter("login");
+                    User user = ManagementSystem.getInstance().getUserByLogin(login);
+                    if (user == null) {
+                        req.setAttribute("errorMessage", "Invalid user or password");
+                        req.getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
+                    } else if (user.getPassword().equals(req.getParameter("password").trim())) {
+                        HttpSession session = req.getSession();
+                        session.setAttribute("user", login);
+                        resp.sendRedirect("/main");
+                        return;
+                    } else {
+                        req.setAttribute("errorMessage", "Invalid user or password");
+                        req.getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
+                    }
+                } else {
+                    req.setAttribute("errorMessage", "Invalid user or password");
+                    req.getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
+                }
+                break;
+            case 2:
+                resp.sendRedirect("/signup");
+                break;
+            default:
+                getServletContext().getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
         }
-        if (answer == 1) {
-                 if (req.getParameter("login") != null) {
-                     String login = req.getParameter("login");
-                     User user = ManagementSystem.getInstance().getUserByLogin(login);
-                     if (user == null) {
-                         req.setAttribute("errorMessage", "Invalid user or password");
-                         req.getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
-                     } else if (user.getPassword().equals(req.getParameter("password").trim())) {
-                         HttpSession session = req.getSession();
-                         session.setAttribute("user", login);
-                         resp.sendRedirect("/main");
-                         return;
-                     } else {
-                         req.setAttribute("errorMessage", "Invalid user or password");
-                         req.getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
-                     }
-                 } else {
-                     req.setAttribute("errorMessage", "Invalid user or password");
-                     req.getRequestDispatcher("/AuthPage.jsp").forward(req, resp);
-                 }
-                 return;
-             }
-        if (answer == 2) {
-            resp.sendRedirect("/signup");
-            return;
-        }
-        getServletContext().getRequestDispatcher("/AuthPage.jsp").forward(req,resp);
     }
 
-    private int checkAction(HttpServletRequest req) throws SQLException {
+    private int checkAction(HttpServletRequest req) {
         if (req.getParameter("Login") != null) {
             return 1;
         }
