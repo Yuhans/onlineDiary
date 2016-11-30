40a1b9c47df1804ca94f088dc2e85db63274bb36
Yuhans <yushin.tema@gmail.com>
2016-11-30 20:04:35 +0300

    add Student, SClass, Subject DAOs

--- src/main/java/com/onlineDiary/logic/ManagementSystem.java
+++ src/main/java/com/onlineDiary/logic/ManagementSystem.java
@@ -1,6 +1,9 @@
 package com.onlineDiary.logic;
 
 import com.onlineDiary.logic.beans.*;
+import com.onlineDiary.logic.dao.SClassDAO;
+import com.onlineDiary.logic.dao.StudentDAO;
+import com.onlineDiary.logic.dao.SubjectDAO;
 import com.onlineDiary.logic.dao.UserDAO;
 
 import javax.naming.Context;
@@ -49,19 +52,16 @@ public class ManagementSystem {
     }
 
     public User getUserByLogin(String login) {
-        UserDAO userDAO = new UserDAO(con);
-        return userDAO.getUser(login);
+        return (new UserDAO(con).getUser(login));
     }
 
 
     public boolean checkLogin(String login) {
-        UserDAO userDAO = new UserDAO(con);
-        return userDAO.isLoginExist(login);
+        return (new UserDAO(con).isLoginExist(login));
     }
 
     public void addUser(User user) {
-        UserDAO userDAO = new UserDAO(con);
-        userDAO.insertUser(user);
+        new UserDAO(con).insertUser(user);
     }
 
     public List<Mark> getMarks(int studId, int subjId) {
@@ -84,133 +84,21 @@ public class ManagementSystem {
         return marks;
     }
 
-    public List getDates() throws SQLException {
-        List dates = new ArrayList();
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM NOVEMBER");
-        while (rs.next()) {
-            dates.add(rs.getDate(1));
-        }
-        rs.close();
-        stmt.close();
-        return dates;
-    }
-
-    public List getSubjects() {
-        List<String> subjects = new ArrayList<>();
-        ResultSet rs = null;
-        try (Statement stmt = con.createStatement()) {
-            rs = stmt.executeQuery("SELECT name FROM SUBJECTS");
-            while (rs.next()) {
-                subjects.add(rs.getString(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return subjects;
-    }
-
     private String getSubjectName(int subjId) {
-        String subjectName = "";
-        //Statement stmt = con.createStatement();
-        ResultSet rs_name = null;
-        try (PreparedStatement pstmt = con.prepareStatement("SELECT name FROM SUBJECTS WHERE id=?")) {
-            pstmt.setInt(1, subjId);
-            rs_name = pstmt.executeQuery();
-            if (rs_name.next()) {
-                subjectName = rs_name.getString(1);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs_name != null) {
-                    rs_name.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return subjectName;
+        return (new SubjectDAO(con).getSubjectNameById(subjId));
     }
 
 
     public List<Student> getStudentsByClass(int classId) {
-        ArrayList<Student> students = new ArrayList<>();
-        ResultSet resultSet = null;
-        try (PreparedStatement ps = con.prepareStatement("SELECT surname, name, patronymic, id FROM students" +
-                " WHERE class_id = ?")) {
-            ps.setInt(1, classId);
-            resultSet = ps.executeQuery();
-            while (resultSet.next()) {
-                String surname = resultSet.getString(1);
-                String name = resultSet.getString(2);
-                String patronymic = resultSet.getString(3);
-                int id = resultSet.getInt(4);
-                students.add(new Student(name, surname, patronymic, classId, id));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return students;
+        return (new StudentDAO(con).getStudents(classId));
     }
 
     public List<Subject> getSubjects(int yearId) {
-        ArrayList<Subject> subjects = new ArrayList<>();
-        ResultSet resultSet = null;
-        try (PreparedStatement ps = con.prepareStatement("SELECT subjects.name, subjects.id FROM subjects_in_classes" +
-                " INNER JOIN subjects ON subject_id = subjects.id WHERE year_id = ?")) {
-            ps.setInt(1, yearId);
-            resultSet = ps.executeQuery();
-            while (resultSet.next()) {
-                String subjectName = resultSet.getString(1);
-                int subjId = resultSet.getInt(2);
-                subjects.add(new Subject(subjectName, subjId));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return subjects;
+        return (new SubjectDAO(con).getSubjectByYear(yearId));
     }
 
     public List<SClass> getClasses() {
-        ArrayList<SClass> classes = new ArrayList<>();
-        try (PreparedStatement ps = con.prepareStatement("SELECT id, year_id, letter FROM classes");
-             ResultSet resultSet = ps.executeQuery()) {
-            while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                int year = resultSet.getInt(2);
-                String letter = resultSet.getString(3);
-                classes.add(new SClass(id, year, letter));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return classes;
+        return (new SClassDAO(con).getAllClasses());
     }
 
     public void addMark(int studId, int subjId, Date date, int mark) {
