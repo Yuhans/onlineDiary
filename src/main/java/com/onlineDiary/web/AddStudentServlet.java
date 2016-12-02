5703a2b76f91cc734cb64b9930997d1e3cc6c4ce
Yuhans <yushin.tema@gmail.com>
2016-12-02 22:26:25 +0300

    some changes in AddStudent

--- src/main/java/com/onlineDiary/web/AddStudentServlet.java
+++ src/main/java/com/onlineDiary/web/AddStudentServlet.java
@@ -3,7 +3,6 @@ package com.onlineDiary.web;
 import com.onlineDiary.logic.ManagementSystem;
 import com.onlineDiary.logic.account.AccountService;
 import com.onlineDiary.logic.beans.Student;
-import com.onlineDiary.web.forms.MainFrameForm;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,30 +16,10 @@ public class AddStudentServlet extends HttpServlet {
     private AccountService accountService = AccountService.getInstance();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         if (isAuthorized(request)) {
-            request.setCharacterEncoding("UTF-8");
-            request.setAttribute("classes", dao.getClasses());
-            MainFrameForm form = new MainFrameForm();
-            int classId = 0;
-            if (request.getParameter("stClass") != null) {
-                classId = Integer.parseInt(request.getParameter("stClass"));
-            }
-            if (request.getParameter("OkB") != null) {
-                String tmp = request.getParameter("name");
-                String tmp2 = request.getParameter("surname");
-                String tmp3 = request.getParameter("patronymic");
-                if (tmp == null || tmp2 == null || tmp3 == null || classId == 0) {
-                    request.setAttribute("submitDone", "no");
-                } else {
-                    String name = request.getParameter("name");
-                    String surname = request.getParameter("surname");
-                    String patronymic = request.getParameter("patronymic");
-                    Student s = new Student(name, surname, patronymic, classId, 0);
-                    dao.addStudent(s);
-                    request.setAttribute("submitDone", "yes");
-                }
-                request.getRequestDispatcher("AddStudent.jsp").forward(request, response);
-            }
+            addStudent(request);
+            request.getRequestDispatcher("AddStudent.jsp").forward(request, response);
         } else {
             response.sendRedirect("/auth");
         }
@@ -56,5 +35,28 @@ public class AddStudentServlet extends HttpServlet {
 
     private boolean isAuthorized(HttpServletRequest request) {
         return accountService.getUserBySessionId(request.getSession().getId()) != null;
+    }
+
+    private void addStudent(HttpServletRequest request) throws ServletException {
+        request.setAttribute("classes", dao.getClasses());
+        int classId = 0;
+        if (request.getParameter("stClass") != null) {
+            classId = Integer.parseInt(request.getParameter("stClass"));
+        }
+        if (request.getParameter("OkB") != null) {
+            String tmp = request.getParameter("name");
+            String tmp2 = request.getParameter("surname");
+            String tmp3 = request.getParameter("patronymic");
+            if (tmp == null || tmp2 == null || tmp3 == null || classId == 0) {
+                request.setAttribute("submitDone", "no");
+            } else {
+                String name = request.getParameter("name");
+                String surname = request.getParameter("surname");
+                String patronymic = request.getParameter("patronymic");
+                Student s = new Student(name, surname, patronymic, classId, 0);
+                dao.addStudent(s);
+                request.setAttribute("submitDone", "yes");
+            }
+        }
     }
 }
