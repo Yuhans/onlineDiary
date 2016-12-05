0d5a48d5ba578c0b9a3f05745fa1321366c9c550
Yuhans <yushin.tema@gmail.com>
2016-12-05 20:59:30 +0300

    add RequestHandler

--- src/main/java/com/onlineDiary/web/AddMarkServlet.java
+++ src/main/java/com/onlineDiary/web/AddMarkServlet.java
@@ -1,33 +1,22 @@
 package com.onlineDiary.web;
 
-import com.onlineDiary.logic.ManagementSystem;
-import com.onlineDiary.logic.Roles;
-import com.onlineDiary.logic.account.AccountService;
-import com.onlineDiary.logic.beans.SClass;
 import com.onlineDiary.web.forms.MainFrameForm;
-import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Date;
 
 public class AddMarkServlet extends HttpServlet {
 
-    private ManagementSystem dao = new ManagementSystem();
-    private AccountService accountService = AccountService.getInstance();
     private MainFrameForm form = new MainFrameForm();
 
-    private static final Logger LOGGER = Logger.getLogger(AddMarkServlet.class);
-    private static final Roles TEACHER = Roles.TEACHER;
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (isAuthorized(request)) {
-            if (isTeacher(request)) {
-                setStudentsAndSubjectsByClass(request);
-                addMark(request);
+        if (RequestHandler.isAuthorized(request)) {
+            if (RequestHandler.isTeacher(request)) {
+                RequestHandler.setStudentsAndSubjectsByClass(request, form);
+                RequestHandler.addMark(request);
                 request.getRequestDispatcher("AddMark.jsp").forward(request, response);
             } else {
                 response.sendRedirect("/main");
@@ -38,9 +27,9 @@ public class AddMarkServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (isAuthorized(request)) {
-            if (isTeacher(request)) {
-                request.setAttribute("classes", dao.getClasses());
+        if (RequestHandler.isAuthorized(request)) {
+            if (RequestHandler.isTeacher(request)) {
+                RequestHandler.setClasses(request);
                 request.getRequestDispatcher("AddMark.jsp").forward(request, response);
             } else {
                 response.sendRedirect("/main");
@@ -50,50 +39,4 @@ public class AddMarkServlet extends HttpServlet {
         }
     }
 
-    private boolean isAuthorized(HttpServletRequest request) {
-        return accountService.getUserBySessionId(request.getSession().getId()) != null;
-    }
-
-    private boolean isTeacher(HttpServletRequest request) {
-        return accountService.getUserBySessionId(request.getSession().getId()).getRole() == TEACHER;
-    }
-
-    private void setStudentsAndSubjectsByClass(HttpServletRequest request) {
-        request.setAttribute("classes", dao.getClasses());
-        int classId = getClassId(request);
-        request.setAttribute("students", dao.getStudentsByClass(classId));
-        SClass schoolClass = dao.getClasses().get(classId - 1);
-        request.setAttribute("subjects", dao.getSubjects(schoolClass.getStudyYear()));
-        schoolClass.setClassId(classId);
-        form.setClassId(schoolClass.getClassId());
-        request.setAttribute("form", form);
-    }
-
-    private void addMark(HttpServletRequest request) {
-        request.setAttribute("submitDone", null);
-        if (request.getParameter("OkB") != null) {
-            String tmp = request.getParameter("studentId");
-            String tmp2 = request.getParameter("subjId");
-            if (tmp == null && tmp2 == null) {
-                request.setAttribute("submitDone", "no");
-                LOGGER.error("Student ID or Subject ID not found");
-            } else {
-                int studId = Integer.parseInt(request.getParameter("studentId"));
-                int subjId = Integer.parseInt(request.getParameter("subjId"));
-                Date date = Date.valueOf(request.getParameter("date"));
-                int mark = Integer.parseInt(request.getParameter("mark"));
-                dao.addMark(studId, subjId, date, mark);
-                LOGGER.info("Mark has been successfully added to student " + studId + ".");
-                request.setAttribute("submitDone", "yes");
-            }
-        }
-    }
-
-    private int getClassId(HttpServletRequest request) {
-        int classId = 1;
-        if (request.getParameter("stClass") != null) {
-            classId = Integer.parseInt(request.getParameter("stClass"));
-        }
-        return classId;
-    }
 }
--- src/main/java/com/onlineDiary/web/MainFrameServlet.java
+++ src/main/java/com/onlineDiary/web/MainFrameServlet.java
@@ -1,9 +1,5 @@
 package com.onlineDiary.web;
 
-import com.onlineDiary.logic.ManagementSystem;
-import com.onlineDiary.logic.Roles;
-import com.onlineDiary.logic.account.AccountService;
-import com.onlineDiary.logic.beans.SClass;
 import com.onlineDiary.web.forms.MainFrameForm;
 
 import javax.servlet.ServletException;
@@ -15,20 +11,11 @@ import java.io.IOException;
 
 public class MainFrameServlet extends HttpServlet {
 
-    private ManagementSystem dao = new ManagementSystem();
     private MainFrameForm form = new MainFrameForm();
-    private AccountService accountService = AccountService.getInstance();
-
-    private static final Roles TEACHER = Roles.TEACHER;
-    private static final Roles STUDENT = Roles.STUDENT;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (isAuthorized(request)) {
-            if (isTeacher(request)) {
-                request.setAttribute("role", TEACHER);
-            } else {
-                request.setAttribute("role", STUDENT);
-            }
+        if (RequestHandler.isAuthorized(request)) {
+            RequestHandler.setRole(request);
             setMainForm(request, response);
         } else {
             response.sendRedirect("/auth");
@@ -36,66 +23,18 @@ public class MainFrameServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (isAuthorized(request)) {
-            if (isTeacher(request)) {
-                request.setAttribute("role", TEACHER);
-            }
-            else {
-                request.setAttribute("role", STUDENT);
-            }
-            setClasses(request, response);
+        if (RequestHandler.isAuthorized(request)) {
+            RequestHandler.setRole(request);
+            RequestHandler.setClasses(request);
+            request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
         } else {
             response.sendRedirect("/auth");
         }
     }
 
-    private boolean isAuthorized(HttpServletRequest request) {
-        return accountService.getUserBySessionId(request.getSession().getId()) != null;
-    }
-
-    private boolean isTeacher(HttpServletRequest request) {
-        return accountService.getUserBySessionId(request.getSession().getId()).getRole() == TEACHER;
-    }
-
     private void setMainForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        setStudentsAndSubjectsByClass(request);
-        setMarks(request);
+        RequestHandler.setStudentsAndSubjectsByClass(request, form);
+        RequestHandler.setMarks(request, form);
         request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
-    }
-
-    private void setMarks(HttpServletRequest request) {
-        if ((request.getParameter("studentId") != null) & (request.getParameter("subjId") != null)) {
-            int studId = Integer.parseInt(request.getParameter("studentId"));
-            int subjId = Integer.parseInt(request.getParameter("subjId"));
-            form.setMarks(dao.getMarks(studId, subjId));
-            form.setSelStudentId(studId);
-            form.setSelSubjId(subjId);
-            request.setAttribute("marks", form.getMarks());
-            request.setAttribute("form", form);
-        }
-    }
-
-    private void setStudentsAndSubjectsByClass(HttpServletRequest request) {
-        request.setAttribute("classes", dao.getClasses());
-        int classId = getClassId(request);
-        request.setAttribute("students", dao.getStudentsByClass(classId));
-        SClass schoolClass = dao.getClasses().get(classId - 1);
-        request.setAttribute("subjects", dao.getSubjects(schoolClass.getStudyYear()));
-        schoolClass.setClassId(classId);
-        form.setClassId(schoolClass.getClassId());
-        request.setAttribute("form", form);
-    }
-
-    private void setClasses(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("classes", dao.getClasses());
-        request.getRequestDispatcher("MainFrame.jsp").forward(request, response);
-    }
-
-    private int getClassId(HttpServletRequest request) {
-        int classId = 1;
-        if (request.getParameter("stClass") != null) {
-            classId = Integer.parseInt(request.getParameter("stClass"));
-        }
-        return classId;
     }
 }
